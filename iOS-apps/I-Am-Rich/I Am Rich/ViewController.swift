cfcb8d2e5eb6d0e694e6d8ef53187b477322ea4b
Dana Huget <danaliyahuget@gmail.com>
2018-02-01 11:11:12 -0800

    Add files via upload

---
+++ iOS-apps/I-Am-Rich/I Am Rich/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  I Am Rich
+//
+//  Created by Dana Huget on 2017-10-02.
+//  Copyright © 2017 Dana Huget. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
