cd5c1f0ee6c8c3bd13088c216bf7fd41af48b113
Dana Huget <danaliyahuget@gmail.com>
2018-02-01 11:01:52 -0800

    Update AppDelegate.swift

--- iOS-apps/Auto-Layout-Calculator/Auto Layout Practice/AppDelegate.swift
+++ iOS-apps/Auto-Layout-Calculator/Auto Layout Practice/AppDelegate.swift
@@ -5,6 +5,8 @@
 //  Created by Angela Yu on 01/12/2016.
 //  Copyright © 2016 London App Brewery. All rights reserved.
 //
+//
+//  Modified by Dana Huget 11/22/2017 (Udemy Course)
 
 import UIKit
 
