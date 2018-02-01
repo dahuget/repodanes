774fd28a4041a97fc79b1e14a56f924832eabcb7
Dana Huget <danaliyahuget@gmail.com>
2018-02-01 10:51:55 -0800

    Add files via upload

---
+++ iOS-apps/Auto-Layout-Calculator/Auto Layout Calculator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Auto Layout Calculator
+//
+//  Created by Dana Huget on 2017-11-22.
+//  Copyright © 2017 Dana Huget. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
